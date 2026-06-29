1dbeff477b3bf4e245dcf098516177f9ebc744fc
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-06-29 22:15:10 +0530

    update user module

--- src/auth/users/users.module.ts
+++ src/auth/users/users.module.ts
@@ -3,9 +3,21 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { Education } from 'src/profile/educations/entities/education.entity';
+import { Experience } from 'src/profile/experiences/entities/experience.entity';
+import { UserTechnology } from 'src/profile/user-technologies/entities/user-technology.entity';
+import { UserTool } from 'src/profile/user-tools/entities/user-tool.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      Education,
+      Experience,
+      UserTechnology,
+      UserTool,
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
